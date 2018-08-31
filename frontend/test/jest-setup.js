76ad094d79f4d44b56bded2d320f9e2ec4ed2da6
Adrian Kreuziger <akreuziger@convoy.com>
2018-08-30 17:21:31 -0700

    Revert "Show pinning message to admins (#8364)"
    This reverts commit bcd8c86b9eb25a0a2bfd6a6d28cb60ab8e07ca26.

--- frontend/test/jest-setup.js
+++ frontend/test/jest-setup.js
@@ -1,5 +1,4 @@
 import "raf/polyfill";
-import "jest-localstorage-mock";
 
 // NOTE: this is needed because sometimes asynchronous code tries to access
 // window.location or similar jsdom properties after the tests have ended and
